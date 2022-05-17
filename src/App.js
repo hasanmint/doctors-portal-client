d487abde2d717d657a9802675c653258fb9643a6
hasanmint73 <hasanmint73@gmail.com>
2022-05-17 21:55:41 +0600

    added navbar menu

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Shared/Navbar';
+import { Route, Routes } from 'react-router-dom';
+import Home from './components/Pages/Home/Home';
+import About from './components/Pages/Home/About';
+import Login from './components/Pages/Authentication/Login';
 
 function App() {
   return (
-    <div className="App">
-      <h1 class="text-3xl font-bold underline">
-        Hello world!
-      </h1>
-    </div>
+    <>
+      <Navbar></Navbar>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </>
   );
 }
 
